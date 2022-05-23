3c713b9f02a51e54373df85d4ddb1afeb776221f
Gaurav <gaurav.yadav1_cs19@gla.ac.in>
2022-05-23 11:28:09 +0530

    gla canteen management system
    Signed-off-by: Gaurav <gaurav.yadav1_cs19@gla.ac.in>

---
+++ lofinpage/loginscript.js
@@ -0,0 +1,55 @@
+function signUp(){
+		
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+    
+    alert("You Have Sign UP SuccessFully");
+    document.getElementById("email").value="";
+    document.getElementById("password").value="";
+}
+
+
+
+function signIn(){
+    
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+    
+    alert("You Have SignIn SuccessFully");    
+    document.getElementById("email").value="";
+    document.getElementById("password").value="";
+    
+}
+
+
+function signOut(){
+    
+    auth.signOut();
+    alert("You Have Loged Out Successfully!");
+    
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        var email = user.email;
+        if (window.location.href.indexOf("index.html") == -1) {
+            window.location.href = "index.html";
+        }
+    } else {
+        
+        if (window.location.href.indexOf("login.html") == -1) {
+            window.location.href = "login.html";
+        }
+    }
+})
+
+
+
+
+
+ 
